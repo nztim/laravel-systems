266d6466429434f282a5055b85582dfa3215e40c
Tim <git@nztm.net>
2022-01-25 11:18:04 +1300

    Update league/html-to-markdown to 5.0

--- src/Mailer/HtmlConverter.php
+++ src/Mailer/HtmlConverter.php
@@ -12,9 +12,9 @@ class HtmlConverter
     {
         $this->converter = $converter;
         $this->converter->setOptions([
+            'remove_nodes'  => 'head',
             'strip_tags'    => true,
             'use_autolinks' => true,
-            'remove_nodes'  => 'head',
         ]);
     }
 
@@ -23,3 +23,9 @@ class HtmlConverter
         return $this->converter->convert($html);
     }
 }
+
+/*
+    LeagueConverter as of 5.0 can throw InvalidArgumentException|RuntimeException if transforming malformed HTML.
+    For now, leave it in place as mail should be queued, and you want to know if there is a problem.
+    But potentially you could log errors and do something else as a backup, such as converting with suppression enabled.
+*/
