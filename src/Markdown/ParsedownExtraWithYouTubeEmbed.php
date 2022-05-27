bfe6a13d06f113530a21d069d9cc7e823285ea00
Tim <git@nztm.net>
2022-05-27 13:51:32 +1200

    Add JotForm embed handler

--- src/Markdown/ParsedownExtraWithYouTubeEmbed.php
+++ src/Markdown/ParsedownExtraWithYouTubeEmbed.php
@@ -21,22 +21,42 @@ class ParsedownExtraWithYouTubeEmbed extends ParsedownExtra
 
     private function handleLink($inline)
     {
+        // If null, it's not a link
         if (!$inline) {
             return $inline;
         }
         $url = $inline['element']['attributes']['href'] ?? '';
-        $code = $this->parseYouTube($url);
-        if (!$code) {
-            return $inline;
+        // YouTube handler
+        $ytCode = $this->parseYouTube($url);
+        if ($ytCode) {
+            return $this->embedYouTube($inline, $ytCode);
         }
+        // JotForm handler
+        $jfCode = $this->parseJotForm($url);
+        if ($jfCode) {
+            return $this->embedJotForm($inline, $jfCode);
+        }
+        // Return unchanged
+        return $inline;
+    }
+
+    private function parseYouTube(string $url): string
+    {
+        $pattern = '#^(?:https?://|//)?(?:www\.|m\.)?(?:youtu\.be/|youtube\.com/(?:embed/|v/|watch\?v=|watch\?.+&v=))([\w-]{11})(?![\w-])#';
+        preg_match($pattern, $url, $matches);
+        return $matches[1] ?? '';
+    }
+
+    public function embedYouTube(array $inline, string $code): array
+    {
         $src = "https://www.youtube.com/embed/{$code}";
         $inline['element'] = [
             'name'       => 'div',
             'position'   => 1,
             'handler'    => 'element',
             'text'       => [
-                'name'    => 'iframe',
-                'text' => '',
+                'name'       => 'iframe',
+                'text'       => '',
                 'attributes' => [
                     'src'             => $src,
                     'frameborder'     => '0',
@@ -50,10 +70,32 @@ class ParsedownExtraWithYouTubeEmbed extends ParsedownExtra
         return $inline;
     }
 
-    private function parseYouTube(string $url): string
+    private function parseJotForm($url): string
     {
-        $pattern = '#^(?:https?://|//)?(?:www\.|m\.)?(?:youtu\.be/|youtube\.com/(?:embed/|v/|watch\?v=|watch\?.+&v=))([\w-]{11})(?![\w-])#';
+        $matches = [];
+        $pattern = '#^https://form\.jotform\.com/(\d*)#';
         preg_match($pattern, $url, $matches);
         return $matches[1] ?? '';
     }
+
+    // <script type="text/javascript" src="https://form.jotform.com/jsform/221398418127055"></script>
+    private function embedJotForm(array $inline, string $code): array
+    {
+        $src = "https://form.jotform.com/jsform/{$code}";
+        $inline['element'] = [
+            'name'     => 'div',
+            'position' => 1,
+            'handler'  => 'element',
+            'text'     => [
+                'name'       => 'script',
+                'text'       => '',
+                'attributes' => [
+                    'type' => 'text/javascript',
+                    'src'  => $src,
+                ],
+            ],
+        ];
+        return $inline;
+    }
 }
+
