e2ae5710cf4558ee4f47e3d487e53783a26b6f54
Tim <git@nztm.net>
2022-08-08 10:30:12 +1200

    Add WhatIsMyIP

---
+++ src/WhatIsMyIp/WhatIsMyIp.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace NZTim\WhatIsMyIp;
+
+use Illuminate\Contracts\Cache\Repository;
+
+class WhatIsMyIp
+{
+    private Repository $cache;
+
+    public function __construct(Repository $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function get(): string
+    {
+        return $this->cache->remember('whatismyip', now()->addDay(), function () {
+            return trim(file_get_contents('https://api.ipify.org'));
+        });
+    }
+}
+
+/*
+ * Alternatively use gethostbyname(config('app.url'))
+ */
