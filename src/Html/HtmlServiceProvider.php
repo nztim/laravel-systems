bf22fa3101304322b0e7687f38e545030bc214f8
Tim <git@nztm.net>
2024-05-19 13:00:24 +1200

    Use class names in service provider

--- src/Html/HtmlServiceProvider.php
+++ src/Html/HtmlServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace NZTim\Html;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Session\Store;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\View\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class HtmlServiceProvider extends ServiceProvider implements DeferrableProvider
@@ -10,7 +13,7 @@ class HtmlServiceProvider extends ServiceProvider implements DeferrableProvider
     public function register()
     {
         $this->app->singleton(FormBuilder::class, function ($app) {
-            return new FormBuilder($app['url'], $app['view'], $app['session.store']);
+            return new FormBuilder($app->make(UrlGenerator::class), $app->make(Factory::class), $app->make(Store::class));
         });
     }
 
